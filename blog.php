7e3422d9527b773abed10149ca1909b9fe438506
ozzi- <oz@oz-web.com>
2016-01-02 13:47:32 +0100

    explanation of config param, refactoring

--- blog.php
+++ blog.php
@@ -1,11 +1,11 @@
 <?php
 	include("blogconfig.php");
 
-	echo('<link rel="stylesheet" type="text/css" href="'.blogCSS.'">');
+	echo('<link rel="stylesheet" type="text/css" href="'.blogCSSPath.'">');
 	echo("<div class='blogTitle'>".blogName."</div><br>");
 
 	$totalPosts=count(glob(blogContentSearchPath));
-	$totalPages=ceil($totalPosts/postsPerPage);	
+	$totalPages=ceil($totalPosts/blogPostsPerPage);	
 	$permaLink=0;
 
 	if (isset($_GET['page'])) {
@@ -16,7 +16,7 @@
 				$page=htmlspecialchars($_GET['page']);
 			}
 		}
-		$firstPost=($totalPosts)-($page*postsPerPage)+postsPerPage;
+		$firstPost=($totalPosts)-($page*blogPostsPerPage)+blogPostsPerPage;
 		$currentPost=$firstPost;
 	}elseif(isset($_GET['permalink'])){
 		if(is_numeric($_GET['permalink'])){
@@ -24,7 +24,7 @@
 		}else{
 			$permaLink="1";
 		}
-		$firstPost=$permaLink+postsPerPage-1;
+		$firstPost=$permaLink+blogPostsPerPage-1;
 		$currentPost=$permaLink;
 		$listPosts=false;
 	}else{
@@ -40,15 +40,15 @@
 	outputPageNav($totalPages,$page,$permaLink);
 	echo("<br>");
 
-	while( ($listPosts==false && $currentPost > $firstPost-postsPerPage) || ($listPosts==true && $currentPost > 0) ) {
+	while( (!$listPosts && $currentPost > $firstPost-blogPostsPerPage) || ($listPosts && $currentPost > 0) ) {
 		$filename=blogContentPath.$currentPost.'.html';
 		if (file_exists($filename)) {
-			if($listPosts==true){
-				if(($pc) % 3 == 0 && $pc!=0){
+			if($listPosts){
+				if(($postCounter) % 3 == 0 && $postCounter!=0){
 					$page=$page+1;
 				}
-				$pc++;
-				echo ('<div class="blogListPostTitle"> <a href="'.blogPath.parameterChar.'page='.($page).'#entry'.$currentPost.'">['.$currentPost.'] - '.file_get_contents(blogContentPath.'dates/'.$currentPost.'.html')."</a> </div><br>");
+				$postCounter++;
+				echo ('<div class="blogListPostTitle"> <a href="'.blogURL.parameterChar.'page='.($page).'#entry'.$currentPost.'">['.$currentPost.'] - '.file_get_contents(blogContentPath.blogContentTitlesPath.$currentPost.'.html')."</a> </div><br>");
 			}else{
 				outputBlogPost($currentPost,$filename);
 			}
@@ -69,9 +69,9 @@
 
 	function outputPageNav($totalPages,$navPage,$permaLink){
 		if($permaLink==0){
-			echo('<center><a href="'.blogPath.parameterChar.'page=1" class="blogPageLink"><<</a>|');
+			echo('<center><a href="'.blogURL.parameterChar.'page=1" class="blogPageLink"><<</a>|');
 			if($totalPages==1){
-				echo('&nbsp;<a href="'.blogPath.parameterChar.'page='.($navPage).'" class="blogPageLink"># '.($navPage).'</a>|');
+				echo('&nbsp;<a href="'.blogURL.parameterChar.'page='.($navPage).'" class="blogPageLink"># '.($navPage).'</a>|');
 			}else{
 				if ($navPage-1>=1){
 					if(!($navPage+1<=$totalPages) && $navPage==$totalPages && ($navPage-2)>=1){
@@ -79,7 +79,7 @@
 					}
 					outputPageLink($navPage-1);
 				}
-				outputPageLink($navPage,true&&$listPosts==false);
+				outputPageLink($navPage,true&&!$listPosts);
 				if ($navPage+1<=$totalPages){
 					outputPageLink($navPage+1);
 					if(!($navPage-1>=1)&&$navPage==1&&($navPage+2<=$totalPages)){
@@ -87,23 +87,23 @@
 					}
 				}
 			}
-			echo('<a href="'.blogPath.parameterChar.'page='.$totalPages.'" class="blogPageLink">>> </a> </center>');
+			echo('<a href="'.blogURL.parameterChar.'page='.$totalPages.'" class="blogPageLink">>> </a> </center>');
 		}
 	}
 
 	function outputBlogPost($currentPost,$filename){
-		echo ('<p id="entry'.$currentPost.'"></p><br><hr><div class="blogPostTitle">['.$currentPost.'] - '.file_get_contents(blogContentPath.'dates/'.$currentPost.'.html')."</div>");
-		echo ('<a href="'.blogPath.parameterChar.'permalink='.($currentPost).'" class="blogPermanentLink">Permanent link</a>');
+		echo ('<p id="entry'.$currentPost.'"></p><br><hr><div class="blogPostTitle">['.$currentPost.'] - '.file_get_contents(blogContentPath.blogContentTitlesPath.$currentPost.'.html')."</div>");
+		echo ('<a href="'.blogURL.parameterChar.'permalink='.($currentPost).'" class="blogPermanentLink">Permanent link</a>');
 		echo("<hr><br>");
 		$blogContent = file_get_contents($filename);
 		echo ('<div class="blogPostContent">'.$blogContent.'</div>');
 	}
 
 	function outputPageLink($i,$bold=false){
-		if($bold==false){
-			echo('&nbsp;<a href="'.blogPath.parameterChar.'page='.($i).'" class="blogPageLink"># '.($i).'</a>|');	
+		if($bold){
+			echo('f&nbsp;<a href="'.blogURL.parameterChar.'page='.($i).'" class="blogCurrentPageLink"><b># '.($i).'</b></a>|');
 		}else{
-			echo('&nbsp;<a href="'.blogPath.parameterChar.'page='.($i).'" class="blogCurrentPageLink"><b># '.($i).'</b></a>|');
+			echo('&nbsp;<a href="'.blogURL.parameterChar.'page='.($i).'" class="blogPageLink"># '.($i).'</a>|');	
 		}
 	}
 	
--- blogconfig.php
+++ blogconfig.php
@@ -1,10 +1,31 @@
 <?php
+	// Title shown on top of the page
 	define ( "blogName", "A few thoughts and tricks" );
-	define ( "postsPerPage", "3" );
-	define ( "blogPath", "index.php?view=blog");
-	define ( "blogContentSearchPath", "./blogcontent/*.*" );
-	define ( "blogCSS", "css/blog.css" );
+
+	// Posts displayed per page
+	define ( "blogPostsPerPage", "3" );
+	
+	// Link to the blog page itself
+	define ( "blogURL", "index.php?view=blog");
+
+	// The character appended after 'blogContentPath'.
+	// Use either ? or &. 
+	// Use ? if your path doesn't have a url parameter itself (index.php)
+	// Use & if your path already has a u url paramter (index.php?view=blog)
+	define ( "parameterChar","&"); 
+	
+	// Path where the blog content lies, should end with /
 	define ( "blogContentPath", "./blogcontent/");
-	define ( "parameterChar","&"); // use either ? or & depending if your blogPath contains one or more parameters or none
+
+	// Path where the blog titles lie, should end with /
+	define ( "blogContentTitlesPath" ,"./blongcontent/titles/");
+
+	// Do not change
+	define ( "blogContentSearchPath", blogContentPath."*.*" );
+	
+	// Path where the css lies
+	define ( "blogCSSPath", "css/blog.css" );
+
+	// Message that is displayed if no content was found
 	define ( "noPostsYet","<hr>There aren't any Posts yet. Make sure to come back soon!<hr>");
 ?>
