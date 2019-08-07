51db3aefcbb11b1eb06c7e762a2fc65a5a4a1f58
ozzi- <oz@oz-web.com>
2019-08-07 16:54:24 +0200

    Update blogconfig.php

--- microblog/blogconfig.php
+++ microblog/blogconfig.php
@@ -5,9 +5,6 @@
 	// Link to the blog page itself
 	define ( "blogURL", "index.php");
 
-	// title used in permalink metadata
-	define ( "blogName", "A few thoughts and tricks");
-
 	// The character appended after 'blogContentPath'.
 	// Use either ? or &.
 	// Use ? if your path doesn't have a url parameter itself (index.php)
