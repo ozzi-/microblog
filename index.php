118e2ed9a5eb84c4ae831994f1b292a0b2461704
ozzi- <oz@oz-web.com>
2019-08-26 13:47:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
   include("microblog/blog.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<?php
+			outputMetaData();
+		?>
+	</head>
+	<body>
+<?php
   renderBlog();
 ?>
+	</body>
+</html>
